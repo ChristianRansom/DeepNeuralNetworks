75b28854ec01aca134394a089a14a259ca71096e
Christian Ransom <christian.ransom123@gmail.com>
2019-02-14 13:00:25 -0800

    Added matrix multiply method

--- Source/matrix.py
+++ Source/matrix.py
@@ -23,6 +23,31 @@ class Matrix():
                 result[row].append(a.data[row][col] + b.data[row][col])
         return Matrix(result)
 
+    @staticmethod
+    def subtract(a, b):
+        '''Assumes that the dimensions of a and b are the same'''
+        result = []
+        for row in range(len(a.data)):
+            result.append([])
+            for col in range(len(a.data[0])):
+                result[row].append(a.data[row][col] - b.data[row][col])
+        return Matrix(result)
+    
+    @staticmethod
+    def multiply(a, b):
+        '''Assumes number of cols in a equals the number of rows in b'''
+        result = []
+        # iterate through rows of a
+        for i in range(len(a.data)):
+            result.append([])
+            # iterate through columns of b
+            for j in range(len(b.data[0])):
+                result[i].append(0)
+                # iterate through rows of b
+                for k in range(len(b.data)):
+                    result[i][j] = result[i][j] + a.data[i][k] * b.data[k][j]
+        return result
+
     def __repr__(self):
         result = ""
         for row in range(len(self.data)):
