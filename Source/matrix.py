a9474659b532ead3d16df492b1762d677e062d57
Christian Ransom <christian.ransom123@gmail.com>
2019-02-23 12:21:34 -0800

    Added transpose function to the matrix library

--- Source/matrix.py
+++ Source/matrix.py
@@ -10,7 +10,8 @@ class Matrix():
     '''
 
     def __init__(self, data):
-        
+        '''The outer list holds a list for each row. Each row list contains the elements
+        in that row'''
         self.data = data #the actual lists of lists storing the numbers
 
     @staticmethod
@@ -81,3 +82,17 @@ def hadamard(a, b):
         for j in range(len(a.data[0])):
             result[i].append(a.data[i][j] * b.data[i][j])
     return Matrix(result)
+
+def transpose(a):
+    '''Transpose swaps the row column position of every element. element[2][3] 
+    is moved to [3][2]
+    This method is static and does not change the original matrix. '''
+    result = []
+
+    for row in range(len(a.data)): # iterate through rows of a
+        for col in range(len(a.data[0])): # iterate through elements in row a
+            if row == 0:
+                result.append([]) #creates a new row 
+            result[col].append(a.data[row][col])
+    return Matrix(result)
+
